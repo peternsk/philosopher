90bbe3a930bb6d142422938a8f0c5e5822760829
peternsk <petermalonge@gmail.com>
2023-12-07 15:03:49 -0500

    philo 1

--- check_sign_arg.c
+++ check_sign_arg.c
@@ -6,8 +6,33 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 13:23:11 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/12/07 13:23:22 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/12/07 14:56:03 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+bool    check_sign_arg(char *str)
+{
+    int i;
+    int sign_count;
+
+    i = 0;
+    sign_count = 0;
+    if(!str)
+        return(false);
+    if(str[i] == '-' || str[i] == '+')
+    {
+        sign_count++;
+        i++;
+    }
+    while(str[i] != '\0')
+    {
+        if(str[i] == '-' || str[i] == '+')
+            return(false);
+        i++;
+    }
+    if(sign_count > 1)
+        return(false);
+    return(true);
+}
--- parser.c
+++ parser.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/24 09:54:10 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/12/07 15:02:55 by pnsaka           ###   ########.fr       */
+/*   Created: 2023/12/07 14:56:44 by pnsaka            #+#    #+#             */
+/*   Updated: 2023/12/07 15:02:31 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-#define PHILO_H
+#include "philo.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <time.h>
-#include <stdbool.h>
+bool    parcer(int ac, char **av)
+{
+    int i;
 
-bool    check_dig_arg(char *str);
-bool    check_sign_arg(char *str);
-bool    parcer(int ac, char **av);
-int    	ft_atoi(char *str);
-
-
-#endif
+    i = 0;
+    while(i < ac)
+    {
+        if(check_dig_arg(av[i]) == false)
+            return(false);
+        if(check_dig_arg(av[i]) == false)
+            return(false);
+        i++;
+    }
+    return(true);
+}
